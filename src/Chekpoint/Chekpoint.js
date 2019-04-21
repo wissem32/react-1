d62c713491a6de3eaa41de8b010b19c9c6b42ca9
wissem32 <wissemmahmoud6@gmail.com>
2019-04-21 11:55:25 +0200

    app-react-condition

--- src/Chekpoint/Chekpoint.js
+++ src/Chekpoint/Chekpoint.js
@@ -1,41 +1,38 @@
-import React ,{ Component } from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import '../index.css';
 //import lion from './lion.jpg' ;
 class Chekpoint extends Component {
-    render() { 
+    render() {
+       // let co = "";
+        let mystyle = {};
         
-       if( this.props.sex==="f")
-       {
-      /*  const mystyle ={
-            fontSize:40,
-                }   */        
-                      
-            
-                
-    
+        if (this.props.sex === "f") {
+            mystyle={
+               borderColor:'yellow' ,
+           }
+        }
+           else {
+             mystyle={
+                borderColor:'blue'
+           }
+        }
+       /*     co = "yellow";
+        } else {
+            co = "blue";
+        }*/
+
         return (
-            <div className="checkpoint-f"  >
-           <center> <img  src={this.props.logo} className="image-f"/>
-            <p><i>nom: {this.props.name}</i></p> 
-           
-            <p>seconde: {this.props.secondename}</p>
-            </center>
-            </div>
-        );
-        }
-        else {
-            return (
-                <div className="checkpoint-m"  >
-               <center> <img  src={this.props.logo} className="image-m"/>
-                <p><i>nom: {this.props.name}</i></p> 
-               
-                <p>seconde: {this.props.secondename}</p>
+            <div className="checkpoint" style={mystyle}/*{{borderColor: co, height: he}}*/ >
+                <center> <img src={this.props.logo} className="image-f" />
+                    <p><i>nom: {this.props.name}</i></p>
+
+                    <p>seconde: {this.props.secondename}</p>
                 </center>
-                </div>
-            );
-        }
-        
-    }
+            </div>
+        )
+
+    
+}
 }
 export default Chekpoint;
